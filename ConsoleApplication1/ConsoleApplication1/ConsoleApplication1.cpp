e70b81f19b85073f605dfc119fc6e73feea9e5cf
ktc25a31a0015 <ktc25a31a0015@edu.kyoto-tech.ac.jp>
2026-04-25 14:27:23 +0900

    作成

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,61 @@
+﻿#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+	int t = (int)time(nullptr);
+	int answer = t % 10;
+
+	int rank = 0;
+
+	bool play = true;
+
+	while (play) {
+		printf("数値を入力して Enter\n");
+		int input;
+		scanf_s("%d", &input);
+
+		if (answer == input) {
+			rank = 2;
+
+			printf("一致！\n");
+		}
+		else {
+			if (answer < input) {
+				printf("大きい！\n");
+			}
+			else {
+				printf("小さい！\n");
+			}
+
+			int sub = answer - input;
+
+			if (-3 < sub && sub < 3) {
+				rank = 1;
+
+				printf("惜しい！\n");
+			}
+		}
+
+		switch (rank) {
+		case 2:
+			play = false;
+
+			printf("ランクS\n");
+
+			break;
+
+		case 1:
+			printf("ランクA\n");
+
+			break;
+
+		default:
+			printf("ランクB\n");
+
+			break;
+		}
+	}
+
+	return 0;
+}
