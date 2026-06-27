afc0a6bad61ba9a70ceb98a0f99bb0fdff668b28
ktc25a31a0015 <ktc25a31a0015@edu.kyoto-tech.ac.jp>
2026-06-27 15:01:47 +0900

    完成

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,61 +1,140 @@
-﻿#include <stdio.h>
-#include <time.h>
+﻿#include <iostream>
+
+bool isDigit(std::string s) {
+    for (char c : s) {
+        if (!std::isdigit(static_cast<unsigned char>(c))) {
+            return false;
+        }
+    }
+
+    return !s.empty();
+}
+
+void clearInput() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+class RandDigits {
+public:
+    RandDigits() {
+        for (int i = 0; i < 4; i++) {
+            digits[i] = rand() % 10;
+        }
+    }
+
+public:
+	int digits[4] = {};
+};
+
+class InputDigits {
+public:
+	int digits[4] = {};
+};
+
+struct Result {
+	bool success;
+	int hit;
+	int blow;
+};
+
+class Compare {
+public:
+	Compare(const RandDigits& rand, const InputDigits& input) {
+		int checkedList[10] = {};
+		int checkedCount = 0;
+
+		for (int r = 0; r < 4; r++) {
+			for (int v = 0; v < 4; v++) {
+				if (rand.digits[r] == input.digits[v]) {
+					bool checked = false;
+
+					for (int i = 0; i < checkedCount; i++) {
+						if (checkedList[i] == rand.digits[r]) {
+							checked = true;
+
+							break;
+						}
+					}
+
+					if (checked) continue;
+
+					checkedList[checkedCount] = rand.digits[r];
+					checkedCount++;
+
+					if (r == v) {
+						result.hit++;
+					}
+					else {
+						result.blow++;
+					}
+				}
+			}
+		}
+
+		result.success = result.hit == 4;
+	};
+
+public:
+	Result result = { false, 0, 0 };
+};
+
+class Disp {
+public:
+	Disp(const RandDigits& rands, Result& result) {
+		if (result.success) {
+			std::cout << "成功！！\n";
+		}
+		else {
+			std::cout << "失敗 (" << rands.digits[0] << rands.digits[1] << rands.digits[2] << rands.digits[3] << ")\n";
+			std::cout << "ヒット : " << result.hit << " 個\n";
+			std::cout << "ブロー : " << result.blow << " 個\n";
+		}
+	};
+};
 
 int main()
 {
-	int t = (int)time(nullptr);
-	int answer = t % 10;
+    srand((unsigned int)time(NULL));
+	
+	int digit = 0;
+	int count = 0;
 
-	int rank = 0;
+	std::string input;
 
-	bool play = true;
+	while (true) {
+		count++;
 
-	while (play) {
-		printf("数値を入力して Enter\n");
-		int input;
-		scanf_s("%d", &input);
+		RandDigits rands;
+		InputDigits inputs;
 
-		if (answer == input) {
-			rank = 2;
+		for (int i = 0; i < 4; i++) {
+			while (true) {
+				std::cout << i + 1 << "桁目の入力 : ";
+				std::cin >> input;
 
-			printf("一致！\n");
-		}
-		else {
-			if (answer < input) {
-				printf("大きい！\n");
+				if (!isDigit(input)) {
+					clearInput();
+
+					continue;
+				}
+
+				try {
+					inputs.digits[i] = input[0] - '0';
+				}
+				catch (...) {
+					continue;
+				}
+
+				break;
 			}
-			else {
-				printf("小さい！\n");
-			}
-
-			int sub = answer - input;
-
-			if (-3 < sub && sub < 3) {
-				rank = 1;
-
-				printf("惜しい！\n");
-			}
 		}
 
-		switch (rank) {
-		case 2:
-			play = false;
+		Compare compare(rands, inputs);
 
-			printf("ランクS\n");
+		Disp disp(rands, compare.result);
 
-			break;
-
-		case 1:
-			printf("ランクA\n");
-
-			break;
-
-		default:
-			printf("ランクB\n");
-
-			break;
-		}
+		if (compare.result.success) break;
 	}
 
-	return 0;
+    std::cout << count << "回で" << "ゲームクリア！\n";
 }
